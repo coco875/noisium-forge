d6455228541111091b4fca26f98c09e38d42c383
coco875 <59367621+coco875@users.noreply.github.com>
2023-12-18 15:48:51 +0100

    finish forge port

--- src/main/java/io/github/steveplays28/noisium/mixin/NoiseChunkGeneratorMixin.java
+++ src/main/java/io/github/steveplays28/noisium/mixin/NoiseChunkGeneratorMixin.java
@@ -3,22 +3,29 @@ package io.github.steveplays28.noisium.mixin;
 import net.minecraft.SharedConstants;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.Registry;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.StructureManager;
 import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.chunk.LevelChunkSection;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
 import net.minecraft.world.level.levelgen.NoiseChunk;
 import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
+import net.minecraft.world.level.levelgen.NoiseSettings;
 import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.levelgen.blending.Blender;
+import net.minecraft.world.level.levelgen.structure.StructureSet;
+import net.minecraft.Util;
 
 import org.spongepowered.asm.mixin.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
@@ -27,10 +34,16 @@ public abstract class NoiseChunkGeneratorMixin extends ChunkGenerator {
 	@Shadow
 	@Final
 	public Holder<NoiseGeneratorSettings> settings;
-
-	public NoiseChunkGeneratorMixin(BiomeSource biomeSource) {
-		super(biomeSource);
+	
+	public NoiseChunkGeneratorMixin(Registry<StructureSet> p_207960_, Optional<HolderSet<StructureSet>> p_207961_,
+			BiomeSource p_207962_) {
+		super(p_207960_, p_207961_, p_207962_);
+		//TODO Auto-generated constructor stub
 	}
+
+	// public NoiseChunkGeneratorMixin(BiomeSource biomeSource) {
+	// 	super(biomeSource);
+	// }
 
 	/**
 	 * @author Steveplays28
@@ -52,17 +65,17 @@ public abstract class NoiseChunkGeneratorMixin extends ChunkGenerator {
 		final int horizontalCellBlockCount = chunkNoiseSampler.cellWidth();
 		final int verticalCellBlockCount = chunkNoiseSampler.cellHeight();
 		final int horizontalCellCount = 16 / horizontalCellBlockCount;
-		final var mutableBlockPos = new BlockPos.Mutable();
+		final var mutableBlockPos = new BlockPos.MutableBlockPos();
 
 		for (int baseHorizontalWidthCellIndex = 0; baseHorizontalWidthCellIndex < horizontalCellCount; ++baseHorizontalWidthCellIndex) {
-			chunkNoiseSampler.sampleEndDensity(baseHorizontalWidthCellIndex);
+			chunkNoiseSampler.advanceCellX(baseHorizontalWidthCellIndex);
 
 			for (int baseHorizontalLengthCellIndex = 0; baseHorizontalLengthCellIndex < horizontalCellCount; ++baseHorizontalLengthCellIndex) {
-				var nextChunkSectionIndex = chunk.countVerticalSections() - 1;
+				var nextChunkSectionIndex = chunk.getSectionsCount() - 1;
 				var chunkSection = chunk.getSection(nextChunkSectionIndex);
 
 				for (int verticalCellHeightIndex = cellHeight - 1; verticalCellHeightIndex >= 0; --verticalCellHeightIndex) {
-					chunkNoiseSampler.onSampledCellCorners(verticalCellHeightIndex, baseHorizontalLengthCellIndex);
+					chunkNoiseSampler.selectCellYZ(verticalCellHeightIndex, baseHorizontalLengthCellIndex);
 
 					for (int verticalCellBlockIndex = verticalCellBlockCount - 1; verticalCellBlockIndex >= 0; --verticalCellBlockIndex) {
 						int blockPosY = (minimumCellY + verticalCellHeightIndex) * verticalCellBlockCount + verticalCellBlockIndex;
@@ -75,28 +88,28 @@ public abstract class NoiseChunkGeneratorMixin extends ChunkGenerator {
 						}
 
 						double deltaY = (double) verticalCellBlockIndex / verticalCellBlockCount;
-						chunkNoiseSampler.interpolateY(blockPosY, deltaY);
+						chunkNoiseSampler.updateForY(blockPosY, deltaY);
 
 						for (int horizontalWidthCellBlockIndex = 0; horizontalWidthCellBlockIndex < horizontalCellBlockCount; ++horizontalWidthCellBlockIndex) {
 							int blockPosX = chunkPosStartX + baseHorizontalWidthCellIndex * horizontalCellBlockCount + horizontalWidthCellBlockIndex;
 							int chunkSectionBlockPosX = blockPosX & 0xF;
 							double deltaX = (double) horizontalWidthCellBlockIndex / horizontalCellBlockCount;
 
-							chunkNoiseSampler.interpolateX(blockPosX, deltaX);
+							chunkNoiseSampler.updateForX(blockPosX, deltaX);
 
 							for (int horizontalLengthCellBlockIndex = 0; horizontalLengthCellBlockIndex < horizontalCellBlockCount; ++horizontalLengthCellBlockIndex) {
 								int blockPosZ = chunkPosStartZ + baseHorizontalLengthCellIndex * horizontalCellBlockCount + horizontalLengthCellBlockIndex;
 								int chunkSectionBlockPosZ = blockPosZ & 0xF;
 								double deltaZ = (double) horizontalLengthCellBlockIndex / horizontalCellBlockCount;
 
-								chunkNoiseSampler.interpolateZ(blockPosZ, deltaZ);
-								BlockState blockState = chunkNoiseSampler.sampleBlockState();
+								chunkNoiseSampler.updateForZ(blockPosZ, deltaZ);
+								BlockState blockState = chunkNoiseSampler.getInterpolatedState();
 
 								if (blockState == null) {
-									blockState = ((NoiseChunkGenerator) (Object) this).settings.value().defaultBlock();
+									blockState = ((NoiseBasedChunkGenerator) (Object) this).settings.value().defaultBlock();
 								}
 
-								if (blockState == NoiseChunkGenerator.AIR || SharedConstants.isOutsideGenerationArea(chunk.getPos())) {
+								if (blockState == NoiseBasedChunkGenerator.AIR || SharedConstants.debugVoidTerrain(chunk.getPos())) {
 									continue;
 								}
 
@@ -105,36 +118,36 @@ public abstract class NoiseChunkGeneratorMixin extends ChunkGenerator {
 								chunkSection.nonEmptyBlockCount += 1;
 
 								if (!blockState.getFluidState().isEmpty()) {
-									chunkSection.nonEmptyFluidCount += 1;
+									chunkSection.tickingFluidCount += 1;
 								}
 
-								if (blockState.hasRandomTicks()) {
-									chunkSection.randomTickableBlockCount += 1;
+								if (blockState.isRandomlyTicking()) {
+									chunkSection.tickingBlockCount += 1;
 								}
 
 								// Set the blockstate in the palette storage directly to improve performance
-								var blockStateId = chunkSection.blockStateContainer.data.palette.index(blockState);
-								chunkSection.blockStateContainer.data.storage().set(
-										chunkSection.blockStateContainer.paletteProvider.computeIndex(chunkSectionBlockPosX,
+								var blockStateId = chunkSection.states.data.palette.idFor(blockState);
+								chunkSection.states.data.storage().set(
+										chunkSection.states.strategy.getIndex(chunkSectionBlockPosX,
 												chunkSectionBlockPosY, chunkSectionBlockPosZ
 										), blockStateId);
 
-								oceanFloorHeightMap.trackUpdate(chunkSectionBlockPosX, blockPosY, chunkSectionBlockPosZ, blockState);
-								worldSurfaceHeightMap.trackUpdate(chunkSectionBlockPosX, blockPosY, chunkSectionBlockPosZ, blockState);
+								oceanFloorHeightMap.update(chunkSectionBlockPosX, blockPosY, chunkSectionBlockPosZ, blockState);
+								worldSurfaceHeightMap.update(chunkSectionBlockPosX, blockPosY, chunkSectionBlockPosZ, blockState);
 
-								if (!aquiferSampler.needsFluidTick() || blockState.getFluidState().isEmpty()) {
+								if (!aquiferSampler.shouldScheduleFluidUpdate() || blockState.getFluidState().isEmpty()) {
 									continue;
 								}
 
 								mutableBlockPos.set(blockPosX, blockPosY, blockPosZ);
-								chunk.markBlockForPostProcessing(mutableBlockPos);
+								chunk.markPosForPostprocessing(mutableBlockPos);
 							}
 						}
 					}
 				}
 			}
 
-			chunkNoiseSampler.swapBuffers();
+			chunkNoiseSampler.swapSlices();
 		}
 
 		chunkNoiseSampler.stopInterpolation();
@@ -147,37 +160,37 @@ public abstract class NoiseChunkGeneratorMixin extends ChunkGenerator {
 	 */
 	@Overwrite
 	@SuppressWarnings("ForLoopReplaceableByForEach")
-	public CompletableFuture<Chunk> populateNoise(Executor executor, Blender blender, NoiseConfig noiseConfig, StructureAccessor structureAccessor, Chunk chunk) {
-		GenerationShapeConfig generationShapeConfig = this.settings.value().generationShapeConfig().trimHeight(chunk.getHeightLimitView());
-		int minimumY = generationShapeConfig.minimumY();
-		int generationShapeHeightFloorDiv = Math.floorDiv(generationShapeConfig.height(), generationShapeConfig.verticalCellBlockCount());
+	public CompletableFuture<ChunkAccess> fillFromNoise(Executor executor, Blender blender, RandomState noiseConfig, StructureManager structureAccessor, ChunkAccess chunk) {
+		NoiseSettings generationShapeConfig = this.settings.value().noiseSettings().clampToHeightAccessor(chunk.getHeightAccessorForGeneration());
+		int minimumY = generationShapeConfig.minY();
+		int generationShapeHeightFloorDiv = Math.floorDiv(generationShapeConfig.height(), generationShapeConfig.getCellHeight());
 
 		if (generationShapeHeightFloorDiv <= 0) {
 			return CompletableFuture.completedFuture(chunk);
 		}
 
-		int minimumYFloorDiv = Math.floorDiv(minimumY, generationShapeConfig.verticalCellBlockCount());
+		int minimumYFloorDiv = Math.floorDiv(minimumY, generationShapeConfig.getCellHeight());
 		int startingChunkSectionIndex = chunk.getSectionIndex(
-				generationShapeHeightFloorDiv * generationShapeConfig.verticalCellBlockCount() - 1 + minimumY);
+				generationShapeHeightFloorDiv * generationShapeConfig.getCellHeight() - 1 + minimumY);
 		int minimumYChunkSectionIndex = chunk.getSectionIndex(minimumY);
-		ArrayList<ChunkSection> chunkSections = new ArrayList<>();
+		ArrayList<LevelChunkSection> chunkSections = new ArrayList<>();
 
 		for (int chunkSectionIndex = startingChunkSectionIndex; chunkSectionIndex >= minimumYChunkSectionIndex; --chunkSectionIndex) {
-			ChunkSection chunkSection = chunk.getSection(chunkSectionIndex);
+			LevelChunkSection chunkSection = chunk.getSection(chunkSectionIndex);
 
-			chunkSection.lock();
+			chunkSection.acquire();
 			chunkSections.add(chunkSection);
 		}
 
 		return CompletableFuture.supplyAsync(
-				Util.debugSupplier("wgen_fill_noise",
-						() -> this.populateNoise(blender, structureAccessor, noiseConfig, chunk, minimumYFloorDiv,
+				Util.wrapThreadWithTaskName("wgen_fill_noise",
+						() -> this.doFill(blender, structureAccessor, noiseConfig, chunk, minimumYFloorDiv,
 								generationShapeHeightFloorDiv
 						)
-				), Util.getMainWorkerExecutor()).whenCompleteAsync((chunk2, throwable) -> {
+				), Util.backgroundExecutor()).whenCompleteAsync((chunk2, throwable) -> {
 			// Replace an enhanced for loop with a fori loop
 			for (int i = 0; i < chunkSections.size(); i++) {
-				chunkSections.get(i).unlock();
+				chunkSections.get(i).release();
 			}
 		}, executor);
 	}
